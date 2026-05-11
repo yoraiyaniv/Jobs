461569d89d39cdec6b6eca1e14c2639bb3d1ce2c
bananamen3 <yoraiyaniv@gmail.com>
2026-05-11 13:08:22 +0300

    secrets file for future secret manager.

--- secrets.py
+++ secrets.py
@@ -1,5 +1,14 @@
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
+
+def gemini_api_key():
+    secret=os.getenv("GEMINI_API_KEY")
+    if secret:
+        return secret
+    else:
+        raise EnvironmentError("Secret dose'nt exist.")
+
+if __name__ == "__main__":
+    print(gemini_api_key())
